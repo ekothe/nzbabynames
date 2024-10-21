92ff35aa91976dffc297ce0be31cea566ce18a40
Sandra <sandra.k.tanz@gmail.com>
2024-10-21 14:28:07 +1100

    updated to 2023

--- data-raw/nzbabynames.R
+++ data-raw/nzbabynames.R
@@ -4,14 +4,15 @@ library(dplyr, warn.conflicts = FALSE)
 library(readr)
 library(usethis)
 
-nzbabynames <- read_csv("https://catalogue.data.govt.nz/dataset/01ee87cd-ecf8-44a1-ad33-b376a689e597/resource/0b0b326c-d720-480f-8f86-bf2d221c7d3f/download/baby-names-2021-01-07.csv",
+nzbabynames <- read_csv("https://catalogue.data.govt.nz/dataset/01ee87cd-ecf8-44a1-ad33-b376a689e597/resource/0b0b326c-d720-480f-8f86-bf2d221c7d3f/download/baby-names-2024-01-08.csv",
                col_types = cols(Sex = col_character()))
 
 nzbabynames <- nzbabynames %>%
   mutate(Sex = case_when(
     Sex == "F" ~ "Female",
     Sex == "M" ~ "Male"
-  ))
+  )) %>%
+  arrange(Sex, Name, Year)
 
 write_csv(nzbabynames, "data-raw/nzbabynames.csv")
 usethis::use_data(nzbabynames, overwrite = TRUE)
